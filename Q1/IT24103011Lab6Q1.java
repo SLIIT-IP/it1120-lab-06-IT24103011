d929b8658b5a5026d3d72ba79f909117a3b349f9
IT24103011 <it24103011@my.sliit.lk>
2024-08-28 07:56:54 +0530

    Add files via upload

---
+++ Q1/IT24103011Lab6Q1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class IT24103011Lab6Q1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        double number = scanner.nextDouble();
+
+        double square = number * number;
+        double squareRoot = Math.sqrt(number);
+
+      
+        System.out.println("The square of " + " " + number + " " + "is : " + square);
+        System.out.println("The square root of " + " " + number + " " + "is : " + squareRoot);
+
+        
+    }
+}
