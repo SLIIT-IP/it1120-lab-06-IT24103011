04cb537685232c2699ca74e79b3e9f69cf33f995
IT24103011 <it24103011@my.sliit.lk>
2024-08-28 08:01:46 +0530

    Add files via upload

---
+++ Q2/IT24103011Lab6Q2A.java
@@ -0,0 +1,10 @@
+public class IT24103011Lab6Q2A {
+    public static void main(String[] args) {
+        int no = 1;
+        while (no <= 10) {
+            System.out.print(no + " ");
+            no++;
+        }
+        System.out.println();
+    }
+}
